8c72a462939f690eba711d5e31c9016775a9c820
Nicolas Sirimongkol <nsirimo@calstatela.edu>
2018-02-19 23:54:20 -0800

    Add foundation for geolocation in home page with logging to console for lat/long

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Geolocation } from '@ionic-native/geolocation';
+
 
 @NgModule({
   declarations: [
@@ -28,6 +30,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    Geolocation,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Geolocation } from '@ionic-native/geolocation';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  latCoords = 0.0;
+  lonCoords = 0.0;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private geolocation: Geolocation) {}
+
+
+
+
+  getLocation(){
+  this.geolocation.getCurrentPosition().then((resp) => {
+    this.latCoords = resp.coords.latitude;
+    this.lonCoords = resp.coords.longitude;
+
+    console.log(`Lat: ${this.latCoords} Long: ${this.lonCoords}`);
+  }).catch((error) => {
+    console.log('Error getting your location!', error);
+  });
 
   }
 
