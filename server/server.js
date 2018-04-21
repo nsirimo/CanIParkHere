41c3c99771de3258d6caf35975b303ff60e7efa9
Nicolas Sirimongkol <nsirimo@calstatela.edu>
2018-04-21 15:15:35 -0700

    Add search by street object

--- server/server.js
+++ server/server.js
@@ -10,6 +10,18 @@ var post = [
     { message: 'hi' }
 ]
 
+var testStreet = [
+    {
+        ShortName: '55th St',
+
+        Coord: [
+            -118.3259215,
+            33.9931439
+        ]
+    }
+
+]
+
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -23,7 +35,7 @@ app.get('/subdistricts', async (req, res) => {
         res.send(subdistricts);
     } catch (error) {
         console.error(error);
-        res.sendStatus(200);
+        res.sendStatus(501);
     }
 });
 
@@ -33,7 +45,7 @@ app.post('/addsubdistrict', (req, res) => {
     var subDist = new Subdistrict(subDistData);
     console.log(subDist);
     subDist.save((err, result) => {
-        if(err){
+        if (err) {
             console.log('Saving User Error');
             res.sendStatus(501);
         } else {
@@ -42,6 +54,16 @@ app.post('/addsubdistrict', (req, res) => {
     });
 });
 
+app.post('/rangeData', async (req, res) => {
+    try {
+        var address = req.body;
+        var subdistricts = await Subdistrict.find({ Streets: address });
+        res.send(subdistricts);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(501);
+    }
+});
 
 mongoose.connect('mongodb://test:test@ds223609.mlab.com:23609/ciphappdb', (err) => {
     if (!err)
