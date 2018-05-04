a7dd37f9178a68dc0c35a421284c2b165d025f94
mirasoldavila13 <mirasoldavila13@aim.com>
2018-05-03 18:32:03 -0700

    contructor that takes the title, description and location

---
+++ client/src/models/place.ts
@@ -0,0 +1,9 @@
+import{ Location } from "./location";
+
+export class Place{
+    constructor(public title: string,
+                public description: string,
+                public location: Location){
+
+    }
+}
